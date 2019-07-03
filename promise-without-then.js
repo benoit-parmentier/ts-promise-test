1610ad8b8da36ace7915a31fbf59933e7284c9b5
Benoît Parmentier <bparmentier@norauto.com>
2019-07-03 13:12:22 +0200

    first commit

---
+++ promise-without-then.js
@@ -0,0 +1,32 @@
+const request = require('request');
+
+function downloadPage(url) {
+    return new Promise((resolve, reject) => {
+        request(url, (error, response, body) => {
+            if (error) reject(error);
+            if (response.statusCode != 200) {
+                reject('Invalid status code <' + response.statusCode + '>');
+            }
+            console.log("downloadPage - end");
+            resolve(body);
+        });
+    });
+}
+
+var p1 = new Promise(function (resolve, reject) {    
+    resolve("Promise 1 !");
+});
+
+var p2 = new Promise(function (resolve, reject) {
+    resolve("Promise 2 !");
+});
+
+p1.then(result => {
+    console.log("result", result);
+})
+
+downloadPage('https://microsoft.com');
+
+p2.then(result => {
+    console.log("result", result);
+})
